c2519e3fb09caa25de30c518eea22935605e08b7
InZl <zliobaite@gmail.com>
2025-02-18 19:50:28 +0200

    editing plots

--- run2_analysis.R
+++ run2_analysis.R
@@ -94,45 +94,49 @@ for (sk in 1:length(dd_variant)){
   
   #NPP
   
+  cc <- 1.3
+  coltrans <- '#80808080'
+  ll <- 2
+  llci <- 0.5
   
   fit_npp <- lm(npp ~ mean_hyp,data = data_sites)
   
   file_name <- paste('plots/',dd_now,'/fig_hyp_npp_',dd_now,'.pdf',sep = '')
   pdf(file_name,width = wd1, height = ht1)
-  plot(data_sites[,'mean_hyp'],data_sites[,'npp'],xlab = 'mean HYP', ylab = 'NPP',cex=0.8, main = 'Mean HYP by species',ylim = c(0,3000),xlim = c(1,3))
-  abline(fit_npp)
+  plot(data_sites[,'mean_hyp'],data_sites[,'npp'],xlab = 'mean HYP sp.', ylab = 'NPP',cex=cc, main = 'Mean HYP by species',ylim = c(0,3000),xlim = c(1,3),col = coltrans,cex.axis = cc,cex.lab = cc,lwd = llci)
+  abline(fit_npp,lwd=ll)
   tx <- paste('R2 =',round(summary(fit_npp)$r.squared,digits = 2))
-  text(2.7,2600,tx)
+  text(2.7,2600,tx,cex=cc)
   dev.off()
   
   fit_npp_den <- lm(npp ~ mean_hyp_den,data = data_sites)
   
   file_name <- paste('plots/',dd_now,'/fig_hyp_npp_den_',dd_now,'.pdf',sep = '')
   pdf(file_name,width = wd1, height = ht1)
-  plot(data_sites[,'mean_hyp_den'],data_sites[,'npp'],xlab = 'mean HYP den', ylab = 'NPP',cex=0.8, main = 'Mean HYP by individuals',ylim = c(0,3000),xlim = c(1,3))
-  abline(fit_npp_den)
+  plot(data_sites[,'mean_hyp_den'],data_sites[,'npp'],xlab = 'mean HYP rel. ab.', ylab = 'NPP',cex=cc, main = 'Mean HYP by rel. abundances',ylim = c(0,3000),xlim = c(1,3),col = coltrans,cex.axis = cc,cex.lab = cc,lwd = llci)
+  abline(fit_npp_den,lwd=ll)
   tx <- paste('R2 =',round(summary(fit_npp_den)$r.squared,digits = 2))
-  text(2.7,2600,tx)
+  text(2.7,2600,tx,cex=cc)
   dev.off()
   
   fit_npp_mass_den <- lm(npp ~ mean_hyp_mass_den,data = data_sites)
   
   file_name <- paste('plots/',dd_now,'/fig_hyp_npp_mass_den_',dd_now,'.pdf',sep = '')
   pdf(file_name,width = wd1, height = ht1)
-  plot(data_sites[,'mean_hyp_mass_den'],data_sites[,'npp'],xlab = 'mean HYP mass den', ylab = 'NPP',cex=0.8, main = 'Mean HYP by biomass',ylim = c(0,3000),xlim = c(1,3))
-  abline(fit_npp_mass_den)
+  plot(data_sites[,'mean_hyp_mass_den'],data_sites[,'npp'],xlab = 'mean HYP biomass', ylab = 'NPP',cex=cc, main = 'Mean HYP by biomass',ylim = c(0,3000),xlim = c(1,3),col = coltrans,cex.axis = cc,cex.lab = cc,lwd = llci)
+  abline(fit_npp_mass_den,lwd=ll)
   tx <- paste('R2 =',round(summary(fit_npp_mass_den)$r.squared,digits = 2))
-  text(2.7,2600,tx)
+  text(2.7,2600,tx,cex=cc)
   dev.off()
   
   fit_npp_ing_den <- lm(npp ~ mean_hyp_ing_den,data = data_sites)
   
   file_name <- paste('plots/',dd_now,'/fig_hyp_npp_ing_den_',dd_now,'.pdf',sep = '')
   pdf(file_name,width = wd1, height = ht1)
-  plot(data_sites[,'mean_hyp_ing_den'],data_sites[,'npp'],xlab = 'mean HYP ing den', ylab = 'NPP',cex=0.8, main = 'Mean HYP by energy intake',ylim = c(0,3000),xlim = c(1,3))
+  plot(data_sites[,'mean_hyp_ing_den'],data_sites[,'npp'],xlab = 'mean HYP en. int.', ylab = 'NPP',cex=cc, main = 'Mean HYP by energy intake',ylim = c(0,3000),xlim = c(1,3),col = coltrans,cex.axis = cc,cex.lab = cc,lwd = llci)
   tx <- paste('R2 =',round(summary(fit_npp_ing_den)$r.squared,digits = 2))
-  abline(fit_npp_ing_den)
-  text(2.7,2600,tx)
+  abline(fit_npp_ing_den,lwd=ll)
+  text(2.7,2600,tx,cex=cc)
   dev.off()
   
   res_npp <-c(round(summary(fit_npp)$r.squared,digits = 2),round(summary(fit_npp_den)$r.squared,digits = 2),round(summary(fit_npp_mass_den)$r.squared,digits = 2),round(summary(fit_npp_ing_den)$r.squared,digits = 2))
@@ -143,51 +147,51 @@ for (sk in 1:length(dd_variant)){
   
   file_name <- paste('plots/',dd_now,'/fig_hyp_prec_',dd_now,'.pdf',sep = '')
   pdf(file_name,width = wd1, height = ht1)
-  plot(data_sites[,'mean_hyp'],data_sites[,'prec'],xlab = 'mean HYP', ylab = 'PREC',cex=0.8, main = 'Mean HYP by species',ylim = c(0,3500),xlim = c(1,3))
+  plot(data_sites[,'mean_hyp'],data_sites[,'prec'],xlab = 'mean HYP sp.', ylab = 'PREC',cex=cc, main = 'Mean HYP by species',ylim = c(0,3500),xlim = c(1,3),col = coltrans,cex.axis = cc,cex.lab = cc,lwd = llci)
   tx <- paste('R2 =',round(summary(fit_prec)$r.squared,digits = 2))
-  abline(fit_prec)
-  text(2.7,3000,tx)
+  abline(fit_prec,lwd=ll)
+  text(2.7,3000,tx,cex=cc)
   dev.off()
   
   fit_prec_log_mass <- lm(prec ~ mean_log_mass, data = data_sites)
   
   file_name <- paste('plots/',dd_now,'/fig_log_mass_prec_',dd_now,'.pdf',sep = '')
   pdf(file_name,width = wd1, height = ht1)
-  plot(data_sites[,'mean_log_mass'],data_sites[,'prec'],xlab = 'mean log MASS', ylab = 'PREC',cex=0.8, main = 'Mean log MASS by species',ylim = c(0,3500),xlim = c(0,800))
+  plot(data_sites[,'mean_log_mass'],data_sites[,'prec'],xlab = 'mean log MASS', ylab = 'PREC',cex=cc, main = 'Mean log MASS by species',ylim = c(0,3500),xlim = c(0,800),col = coltrans,cex.axis = cc,cex.lab = cc,lwd = llci)
   tx <- paste('R2 =',round(summary(fit_prec)$r.squared,digits = 2))
   tx <- paste('R2 =',round(summary(fit_prec_log_mass)$r.squared,digits = 2))
-  abline(fit_prec_log_mass)
-  text(2.7,3000,tx)
+  abline(fit_prec_log_mass,lwd=ll)
+  text(2.7,3000,tx,cex=cc)
   dev.off()
   
   fit_prec_den <- lm(prec ~ mean_hyp_den ,data = data_sites)
   
   file_name <- paste('plots/',dd_now,'/fig_hyp_prec_den_',dd_now,'.pdf',sep = '')
   pdf(file_name,width = wd1, height = ht1)
-  plot(data_sites[,'mean_hyp_den'],data_sites[,'prec'],xlab = 'mean HYP', ylab = 'PREC',cex=0.8, main = 'Mean HYP by individuals',ylim = c(0,3500),xlim = c(1,3))
+  plot(data_sites[,'mean_hyp_den'],data_sites[,'prec'],xlab = 'mean HYP rel. ab.', ylab = 'PREC',cex=cc, main = 'Mean HYP by rel. abundances',ylim = c(0,3500),xlim = c(1,3),col = coltrans,cex.axis = cc,cex.lab = cc,lwd = llci)
   tx <- paste('R2 =',round(summary(fit_prec_den)$r.squared,digits = 2))
-  text(2.7,3000,tx)
-  abline(fit_prec_den)
+  text(2.7,3000,tx,cex=cc)
+  abline(fit_prec_den,lwd=ll)
   dev.off()
   
   fit_prec_mass_den <- lm(prec ~ mean_hyp_mass_den,data = data_sites)
   
   file_name <- paste('plots/',dd_now,'/fig_hyp_prec_mass_den_',dd_now,'.pdf',sep = '')
   pdf(file_name,width = wd1, height = ht1)
-  plot(data_sites[,'mean_hyp_mass_den'],data_sites[,'prec'],xlab = 'mean HYP', ylab = 'PREC',cex=0.8, main = 'Mean HYP by biomass',ylim = c(0,3500),xlim = c(1,3))
+  plot(data_sites[,'mean_hyp_mass_den'],data_sites[,'prec'],xlab = 'mean HYP biomass', ylab = 'PREC',cex=cc, main = 'Mean HYP by biomass',ylim = c(0,3500),xlim = c(1,3),col = coltrans,cex.axis = cc,cex.lab = cc,lwd = llci)
   tx <- paste('R2 =',round(summary(fit_prec_mass_den)$r.squared,digits = 2))
-  text(2.7,3000,tx)
-  abline(fit_prec_mass_den)
+  text(2.7,3000,tx,cex=cc)
+  abline(fit_prec_mass_den,lwd=ll)
   dev.off()
   
   fit_prec_ing_den <- lm(prec ~ mean_hyp_ing_den,data = data_sites)
   
   file_name <- paste('plots/',dd_now,'/fig_hyp_prec_ing_den_',dd_now,'.pdf',sep = '')
   pdf(file_name,width = wd1, height = ht1)
-  plot(data_sites[,'mean_hyp_ing_den'],data_sites[,'prec'],xlab = 'mean HYP', ylab = 'PREC',cex=0.8, main = 'Mean HYP by energy intake',ylim = c(0,3500),xlim = c(1,3))
+  plot(data_sites[,'mean_hyp_ing_den'],data_sites[,'prec'],xlab = 'mean HYP en. int.', ylab = 'PREC',cex=cc, main = 'Mean HYP by energy intake',ylim = c(0,3500),xlim = c(1,3),col = coltrans,cex.axis = cc,cex.lab = cc,lwd = llci)
   tx <- paste('R2 =',round(summary(fit_prec_ing_den)$r.squared,digits = 2))
-  text(2.7,3000,tx)
-  abline(fit_prec_ing_den)
+  text(2.7,3000,tx,cex=cc)
+  abline(fit_prec_ing_den,lwd=ll)
   dev.off()
   
   res_prec <-c(round(summary(fit_prec)$r.squared,digits = 2),round(summary(fit_prec_den)$r.squared,digits = 2),round(summary(fit_prec_mass_den)$r.squared,digits = 2),round(summary(fit_prec_ing_den)$r.squared,digits = 2))
@@ -198,50 +202,50 @@ for (sk in 1:length(dd_variant)){
   
   file_name <- paste('plots/',dd_now,'/fig_hyp_temp_',dd_now,'.pdf',sep = '')
   pdf(file_name,width = wd1, height = ht1)
-  plot(data_sites[,'mean_hyp'],data_sites[,'temp'],xlab = 'mean HYP', ylab = 'TEMP',cex=0.8, main = 'Mean HYP by species',ylim = c(-15,35),xlim = c(1,3))
+  plot(data_sites[,'mean_hyp'],data_sites[,'temp'],xlab = 'mean HYP sp.', ylab = 'TEMP',cex=cc, main = 'Mean HYP by species',ylim = c(-15,35),xlim = c(1,3),col = coltrans,cex.axis = cc,cex.lab = cc,lwd = llci)
   tx <- paste('R2 =',round(summary(fit_temp)$r.squared,digits = 2))
-  abline(fit_temp)
-  text(1.5,-12,tx)
+  abline(fit_temp,lwd=ll)
+  text(1.5,-12,tx,cex=cc)
   dev.off()
   
   fit_temp_log_mass <- lm(temp ~ mean_log_mass, data = data_sites)
   
   file_name <- paste('plots/',dd_now,'/fig_log_mass_temp_',dd_now,'.pdf',sep = '')
   pdf(file_name,width = wd1, height = ht1)
-  plot(data_sites[,'mean_log_mass'],data_sites[,'temp'],xlab = 'mean log MASS', ylab = 'TEMP',cex=0.8, main = 'Mean log MASS by species',ylim = c(-15,35),xlim = c(0,800))
+  plot(data_sites[,'mean_log_mass'],data_sites[,'temp'],xlab = 'mean log MASS', ylab = 'TEMP',cex=cc, main = 'Mean log MASS by species',ylim = c(-15,35),xlim = c(0,800),col = coltrans,cex.axis = cc,cex.lab = cc,lwd = llci)
   tx <- paste('R2 =',round(summary(fit_temp_log_mass)$r.squared,digits = 2))
-  abline(fit_temp_log_mass)
-  text(600,-12,tx)
+  abline(fit_temp_log_mass,lwd=ll)
+  text(600,-12,tx,cex=cc)
   dev.off()
   
   fit_temp_den <- lm(temp ~ mean_hyp_den ,data = data_sites)
   
   file_name <- paste('plots/',dd_now,'/fig_hyp_temp_den_',dd_now,'.pdf',sep = '')
   pdf(file_name,width = wd1, height = ht1)
-  plot(data_sites[,'mean_hyp_den'],data_sites[,'temp'],xlab = 'mean HYP den', ylab = 'TEMP',cex=0.8, main = 'Mean HYP by individuals',ylim = c(-15,35),xlim = c(1,3))
+  plot(data_sites[,'mean_hyp_den'],data_sites[,'temp'],xlab = 'mean HYP rel. ab.', ylab = 'TEMP',cex=cc, main = 'Mean HYP by rel. abundances',ylim = c(-15,35),xlim = c(1,3),col = coltrans,cex.axis = cc,cex.lab = cc,lwd = llci)
   tx <- paste('R2 =',round(summary(fit_temp_den)$r.squared,digits = 2))
-  abline(fit_temp_den)
-  text(1.5,-12,tx)
+  abline(fit_temp_den,lwd=ll)
+  text(1.5,-12,tx,cex=cc)
   dev.off()
   
   fit_temp_mass_den <- lm(temp ~ mean_hyp_mass_den ,data = data_sites)
   
   file_name <- paste('plots/',dd_now,'/fig_hyp_temp_mass_den_',dd_now,'.pdf',sep = '')
   pdf(file_name,width = wd1, height = ht1)
-  plot(data_sites[,'mean_hyp_mass_den'],data_sites[,'temp'],xlab = 'mean HYP mass den', ylab = 'TEMP',cex=0.8, main = 'Mean HYP by biomass',ylim = c(-15,35),xlim = c(1,3))
+  plot(data_sites[,'mean_hyp_mass_den'],data_sites[,'temp'],xlab = 'mean HYP biomass', ylab = 'TEMP',cex=cc, main = 'Mean HYP by biomass',ylim = c(-15,35),xlim = c(1,3),col = coltrans,cex.axis = cc,cex.lab = cc,lwd = llci)
   tx <- paste('R2 =',round(summary(fit_temp_mass_den)$r.squared,digits = 2))
-  abline(fit_temp_mass_den)
-  text(1.5,-12,tx)
+  abline(fit_temp_mass_den,lwd=ll)
+  text(1.5,-12,tx,cex=cc)
   dev.off()
   
   fit_temp_ing_den <- lm(temp ~ mean_hyp_ing_den ,data = data_sites)
   
   file_name <- paste('plots/',dd_now,'/fig_hyp_temp_ing_den_',dd_now,'.pdf',sep = '')
   pdf(file_name,width = wd1, height = ht1)
-  plot(data_sites[,'mean_hyp_ing_den'],data_sites[,'temp'],xlab = 'mean HYP ing den', ylab = 'TEMP',cex=0.8, main = 'Mean HYP by energy intake',ylim = c(-15,35),xlim = c(1,3))
+  plot(data_sites[,'mean_hyp_ing_den'],data_sites[,'temp'],xlab = 'mean HYP en. int.', ylab = 'TEMP',cex=cc, main = 'Mean HYP by energy intake',ylim = c(-15,35),xlim = c(1,3),col = coltrans,cex.axis = cc,cex.lab = cc,lwd = llci)
   tx <- paste('R2 =',round(summary(fit_temp_ing_den)$r.squared,digits = 2))
-  abline(fit_temp_ing_den)
-  text(1.5,-12,tx)
+  abline(fit_temp_ing_den,lwd=ll)
+  text(1.5,-12,tx,cex=cc)
   dev.off()
   
   res_temp <-c(round(summary(fit_temp)$r.squared,digits = 2),round(summary(fit_temp_den)$r.squared,digits = 2),round(summary(fit_temp_mass_den)$r.squared,digits = 2),round(summary(fit_temp_ing_den)$r.squared,digits = 2))
